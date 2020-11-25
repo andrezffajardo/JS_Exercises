4eefd80206c583f3083c6c0ca449ed855b6a2928
afajardo-sj <andrezffajardo@gmail.com>
2020-11-25 17:12:09 -0500

    More Data types

--- first_program/index.js
+++ first_program/index.js
@@ -2,5 +2,23 @@ console.log("Hola Mundo");
 let myfirstvariable = "My First Variable!";
 console.log(myfirstvariable);
 
+// mutability
 myfirstvariable = "It's Change";
 console.log(myfirstvariable);
+
+// boolean
+let myBoolean = true;
+let myOtherBool = false;
+
+let myNumber = 0;
+let myNumber2 = 12;
+let myNumber3 = -258;
+
+console.log(myNumber, myNumber2, myBoolean, myfirstvariable);
+
+let undef;
+console.log(undef);
+
+let nulo = null;
+
+console.log(nulo);
