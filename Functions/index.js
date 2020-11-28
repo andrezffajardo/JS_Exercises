c8e84fa1d8ea6034cad897db25fa153b0fbb8564
afajardo-sj <andrezffajardo@gmail.com>
2020-11-27 22:31:28 -0500

    JS Functions More

--- Functions/index.js
+++ Functions/index.js
@@ -1,4 +1,4 @@
-function iterator(arg1) {
+/* function iterator(arg1) {
   for (let i = 0; i < arg1.length; i++) {
     console.log(arg1[i]);
   }
@@ -8,4 +8,14 @@ const numbers = [1, 2, "hola", 4, 5];
 const names = ["George", "Jonas", "Philip", "Andrew", "Carl"];
 
 iterator(numbers);
-iterator(names);
+iterator(names); */
+
+function addition(a, b) {
+  return a + b;
+}
+
+const resultaddition1 = addition(1, 2);
+const resultaddition2 = addition(3, 4);
+const resultaddition3 = addition(resultaddition1, resultaddition2);
+
+console.log("result", resultaddition3);
