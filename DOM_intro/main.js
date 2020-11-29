83db25f465ad3fcce79f356de75aa110fe171275
afajardo-sj <andrezffajardo@gmail.com>
2020-11-28 20:04:09 -0500

    JS App, Get the value of a text field

--- DOM_intro/main.js
+++ DOM_intro/main.js
@@ -1,1 +1,10 @@
-window.onload = () => {};
+window.onload = () => {
+  const form = document.getElementById("to-do-form");
+  form.onsubmit = (e) => {
+    e.preventDefault();
+    const todo = document.getElementById("to-do");
+    const todoText = todo.value;
+    todo.value = "";
+    console.log(todoText);
+  };
+};
