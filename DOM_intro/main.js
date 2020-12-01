dfbbd0297a4bbc9e2faeb269965bc1b6e4c14291
afajardo-sj <andrezffajardo@gmail.com>
2020-11-30 19:50:09 -0500

    JS App, remove elements, persist data

--- DOM_intro/main.js
+++ DOM_intro/main.js
@@ -1,4 +1,4 @@
-const todos = [];
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
 
 const render = () => {
   const todoList = document.getElementById("todo-list");
@@ -9,12 +9,19 @@ const render = () => {
     element.addEventListener("click", () => {
       element.parentNode.removeChild(element);
       todos.splice(i, 1);
+      updateTodos(todos);
       render();
     });
   });
 };
 
+const updateTodos = (todos) => {
+  const todoStrings = JSON.stringify(todos);
+  localStorage.setItem("todos", todoStrings);
+};
+
 window.onload = () => {
+  render();
   const form = document.getElementById("to-do-form");
   form.onsubmit = (e) => {
     e.preventDefault();
@@ -22,6 +29,7 @@ window.onload = () => {
     const todoText = todo.value;
     todo.value = "";
     todos.push(todoText);
+    updateTodos(todos);
     render();
   };
 };
