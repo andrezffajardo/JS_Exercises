452fea4e5837a71692bcbaf63d71a5a08bd8949e
afajardo-sj <andrezffajardo@gmail.com>
2020-12-03 23:51:19 -0500

    JS learning, Conditional statements

--- learning1/app.js
+++ learning1/app.js
@@ -1,1 +1,11 @@
 document.write("<h1>Hello World</h1>");
+
+let score = 10;
+
+if (score > 30) {
+  console.log("You need to practice more");
+} else if (score > 15) {
+  console.log("Ok good job");
+} else {
+  console.log("You need to follow this tutorial");
+}
