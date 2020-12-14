2b7300875da004e02bc5f4fe29a04c61ad16638a
afajardo-sj <andrezffajardo@gmail.com>
2020-12-14 18:10:53 -0500

    JS learning, Conditional statements Switch

--- learning1/app.js
+++ learning1/app.js
@@ -1,4 +1,4 @@
-let score = 10;
+/*let score = 10;
 
 if (score > 30) {
   console.log("You need to practice more");
@@ -6,4 +6,17 @@ if (score > 30) {
   console.log("Ok good job");
 } else {
   console.log("You need to follow this tutorial");
+}*/
+
+let typeCard = "Credit Card";
+
+switch (typeCard) {
+  case "Debit Card":
+    console.log("This is a debit card");
+    break;
+  case "Credit Card":
+    console.log("This is a credit card");
+    break;
+  default:
+    console.log("No Card");
 }
