fcd022420dc0e12789b336d21faa2eae6e61c91a
afajardo-sj <andrezffajardo@gmail.com>
2020-12-17 21:23:51 -0500

    JS learning, function slice() Example

--- learning_platzi2/app.js
+++ learning_platzi2/app.js
@@ -1,6 +1,6 @@
 let name = "Andres Fajardo";
-let lastLetter = name.charAt(name.length - 1);
-//let lastLetter = name.slice(-1);
+//let lastLetter = name.charAt(name.length - 1);
+let lastLetter = name.slice(-1);
 //let lastLetter = name.substr(-1);
 
 console.log(lastLetter);
