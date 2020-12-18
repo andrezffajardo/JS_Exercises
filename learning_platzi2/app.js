83d0cf3c582595c0404947e8ef68c66147d004d3
afajardo-sj <andrezffajardo@gmail.com>
2020-12-18 18:53:54 -0500

    JS learning, example with objects

--- learning_platzi2/app.js
+++ learning_platzi2/app.js
@@ -5,11 +5,33 @@ let lastLetter = name.substr(-1);
 
 console.log(lastLetter);*/
 
-let name = "Andres";
+/*let name = "Andres";
 
 function printNameUppercase(n) {
   n = n.toUpperCase();
   console.log(n);
 }
 
-printNameUppercase(name);
+printNameUppercase(name);*/
+
+//objects
+
+var andres = {
+  name: "Andres",
+  lastName: "Fajardo",
+  age: 40,
+};
+
+var jose = {
+  name: "Jose",
+  lastName: "Caicedo",
+  age: 38,
+};
+
+function printNameUppercase(name) {
+  name = name.toUpperCase();
+  console.log(name);
+}
+
+printNameUppercase(andres.name);
+printNameUppercase(jose.name);
