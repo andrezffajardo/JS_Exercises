89c88c52932784b64c2f32917143d445d827bfd8
afajardo-sj <andrezffajardo@gmail.com>
2020-12-18 18:32:40 -0500

    JS learning, function and array

--- learning_platzi2/app.js
+++ learning_platzi2/app.js
@@ -1,6 +1,15 @@
-let name = "Andres Fajardo";
-//let lastLetter = name.charAt(name.length - 1);
-//let lastLetter = name.slice(-1);
+/*let name = "Andres Fajardo";
+let lastLetter = name.charAt(name.length - 1);
+let lastLetter = name.slice(-1);
 let lastLetter = name.substr(-1);
 
-console.log(lastLetter);
+console.log(lastLetter);*/
+
+let name = "Andres";
+
+function printNameUppercase(n) {
+  n = n.toUpperCase();
+  console.log(n);
+}
+
+printNameUppercase(name);
