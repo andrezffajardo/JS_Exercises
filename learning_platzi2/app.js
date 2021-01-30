8eea7a40b32a57c9424337213e6745d80086fe4c
afajardo-sj <andrezffajardo@gmail.com>
2021-01-29 22:48:13 -0500

    JS learning, Asynchronous JS, async order of execution

--- learning_platzi2/app.js
+++ learning_platzi2/app.js
@@ -482,15 +482,26 @@ const API_URL = "https://swapi.dev/api/";
 const PEOPLE_URL = "people/:id";
 const opts = { crossDomain: true };
 
-const onCharacterResponse = function (character) {
-  console.log(`Hola, yo soy ${character.name}`);
-};
-
-function getCharacter(id) {
+function getCharacter(id, callback) {
   const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`;
-  $.get(url, opts, onCharacterResponse);
+
+  $.get(url, opts, function (character) {
+    console.log(`Hola, yo soy ${character.name}`);
+
+    if (callback) {
+      callback();
+    }
+  });
 }
 
-getCharacter(1);
-getCharacter(2);
-getCharacter(3);
+getCharacter(1, function () {
+  getCharacter(2, function () {
+    getCharacter(3, function () {
+      getCharacter(4, function () {
+        getCharacter(5, function () {
+          getCharacter(6);
+        });
+      });
+    });
+  });
+});
